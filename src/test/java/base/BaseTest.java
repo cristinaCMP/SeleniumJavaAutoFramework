d7a7092e874a31e9fda5078d43c66c82751b5dc0
cpichiu <cristina.m.pichiu@gmail.com>
2024-03-18 10:03:27 +0200

    delete import

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
