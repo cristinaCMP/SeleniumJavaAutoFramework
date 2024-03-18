814760d22ed1536cccf0aaca42f1884f17c8f43c
cpichiu <cristina.m.pichiu@gmail.com>
2024-03-18 10:44:44 +0200

    add setup for firefox

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BaseTest {
 
@@ -13,14 +15,25 @@ public class BaseTest {
 
     public void setup() {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--disable-gpu");
-        chromeOptions.addArguments("--windows-size=1920,1880");
-        chromeOptions.addArguments("--no-sandbox");
+        // run with your browser
+        // System.setProperty("webdriver.firefox.driver","src/test/resources/Firefox.exe");
+        // driver = new FirefoxDriver();
 
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(chromeOptions);
+        // setup for ci/cd running
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--headless");
+//        chromeOptions.addArguments("--disable-gpu");
+//        chromeOptions.addArguments("--windows-size=1920,1880");
+//        chromeOptions.addArguments("--no-sandbox");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--windows-size=1920,1880");
+        firefoxOptions.addArguments("--no-sandbox");
+
+        WebDriverManager.chromedriver().setup(); // search in the computer if the driver exists and if not found it will download for us and running
+//        driver = new ChromeDriver(chromeOptions); //send the chromeOptions as paramether for running in ci/cd
+        driver = new FirefoxDriver(firefoxOptions); //send the chromeOptions as paramether for running in ci/cd
 
         driver.manage().window().maximize();
 
